944e772e80e48db6e1bb7a64859daf73af6a4830
Martin Kiss <martin.kiss@me.com>
2013-02-16 12:21:31 +0100

    Add comments and update README

--- Example/ANPCornerView.m
+++ Example/ANPCornerView.m
@@ -24,8 +24,27 @@
         self.layer.cornerRadius = cornerRadius;
     }
     else {
+        // IMPORTANT: In case of delayed animation, the animation block (and thus also this setter) is invoked immediately without the delay. This is normal behavior of UIView block animations. Because of this, your CAAnimation should be properly delayed.
+        
         CABasicAnimation *animation = [[UIView currentAnimation] basicAnimationForKeypath:@"cornerRadius" toValue:@(cornerRadius)];
         [self.layer addAnimation:animation forKey:@"setCornerRadius:"];
+        
+        
+        
+        // This is how you set up the animation yourself:
+        /*
+         NSTimeInterval delay = [[UIView currentAnimation] delay];
+         NSTimeInterval duration = [[UIView currentAnimation] duration];
+         CAMediaTimingFunction *timingFunction = [[UIView currentAnimation] timingFunction];
+         
+         CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"cornerRadius"];
+         animation.duration = duration + delay; // This is how CAAnimation timing works.
+         animation.beginTime = CACurrentMediaTime() + delay;
+         animation.timingFunction = timingFunction;
+         animation.toValue = @(cornerRadius);
+         animation.fillMode = kCAFillModeForwards;
+         animation.removedOnCompletion = NO;
+         */
     }
 }
 
